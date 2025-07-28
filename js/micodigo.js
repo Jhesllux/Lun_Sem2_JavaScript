e1f64567f7830a1775c02be7eed01ac3af9dd650
Jhesllux <jhonestebansar@gmail.com>
2025-07-28 17:24:47 -0500

    Add files via upload

---
+++ js/micodigo.js
@@ -0,0 +1,17 @@
+function nombre() {
+    document.getElementById("nombre").innerHTML = "Jhon Esteban";
+}
+
+function apellido() {
+    document.getElementById("apellido").innerHTML = "Soto Arroyave";
+}
+
+function celular() {
+    document.getElementById("celular").innerHTML = "3148582165";
+}
+
+function Borrar() {
+    document.getElementById("nombre").innerHTML = "";
+    document.getElementById("apellido").innerHTML = "";
+    document.getElementById("celular").innerHTML = "";
+}
